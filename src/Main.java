4da0e729984719df278d1f0cbc85dbb0c32195d4
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-01-01 14:49:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-      /* add your code here */
+      public class Main {
+    public static void main(String [] args){
+        int [][][] nums = {
+            {
+                {3, 5, 8}, {3, 2, 1}
+            }, 
+
+            {
+                {5, 2, 4}, {6, 2, 7}
+            }, 
+
+            {
+                {7, 9, 9}, {8, 2, 1}
+            }
+        }; 
+        int [][] nums2 = {{7, 8, 9}, {4, 5, 6}};   
+        System.out.println(nums2[1][1]);
+    } 
+}
+
+    
+
     }
 
 }
